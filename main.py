9fab8aa33994f95d2f5cc5cd934052241db15cd8
fighting58 <40029469+fighting58@users.noreply.github.com>
2024-11-08 08:37:06 +0900

    modified

--- main.py
+++ main.py
@@ -1,60 +1,157 @@
-from PyQt5.QtWidgets import QApplication, QFileDialog, QWidget
-from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QFileDialog, QWidget, QPushButton
+from PyQt5.QtCore import Qt, QSize, QRegExp, QFile, QTextStream
+from PyQt5.QtGui import QIcon, QPixmap, QRegExpValidator, QCursor
 import sys
-from shp_transform import calculate_dxdy, calculate_length_and_bearing, adjust_shapefile_features
+from shp_convert import calculate_dxdy, calculate_length_and_bearing, adjust_shapefile_features
+from main_ui import Ui_Form
+import resources
+from QCustomModals import QCustomModals
+import os
+import tempfile
 
-form_class = uic.loadUiType('main.ui')[0]
 
-class ShpTransformer(QWidget, form_class):
+class ShpConverter(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
+        self.setFixedSize(480, 290)
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)  # 테두리 없는 윈도우 생성
+        self.setAttribute(Qt.WA_TranslucentBackground) # 투명 배경 설정
         self.setupUi(self) 
-        self.shp = None
-        self.saveas = None
 
+        self.shp = None
+        self.saveas = None        
+        
+        self.input_path1.setPlaceholderText("Shp 파일 입력")
+        self.input_button1.icon_normal = QIcon(':/images/shapefile_dark.svg')
+        self.input_button1.icon_hover = QIcon(':/images/shapefile_white.svg')
+        self.input_button1.setToolTip("Shp파일 불러오기")
+        self.input_button1.refresh()
+        self.main_frame.setFixedSize(480,290)
+        self.main_frame.setFixedSize(480,290)
+        self.round = 10
+
+        # 좌표 입력란에 validator 설정
+        regExp = QRegExp(r'^[0-9]*\.?[0-9]*$')
+        validator = QRegExpValidator(regExp)
+        for lineedit in [self.px1, self.px2, self.py1, self.py2, self.qx1, self.qx2, self.qy1, self.qy2]:
+            lineedit.setValidator(validator)
+
+        # 파일 입력 및 실행버튼 클릭 시
         self.input_button1.clicked.connect(self.get_shp)
         self.btn_run.clicked.connect(self.run)
+        self.titlebar.help_btn.clicked.connect(self.open_pdf)
+
+        # 스타일 적용
+        style_file = QFile(':/styles/styles.qss')
+        style_file.open(QFile.ReadOnly| QFile.Text)
+        style_stream = QTextStream(style_file)
+        self.setStyleSheet(style_stream.readAll())
 
     def get_shp(self):
+        """ 파일 불러오기 """
         shp, _ = QFileDialog.getOpenFileName(self, caption="Select shp file", directory='', filter='shp file(*.shp)')
         if shp:
             self.input_path1.setText(shp)
             self.shp = shp
-            self.saveas = shp.replace(".shp", "_transformed.shp")
+            self.saveas = shp.replace(".shp", "_converted.shp")
+        else:
+            self.shp = None
+            self.input_path1.setText('')
     
+    def show_modal(self, modal_type, **kargs):
+        """ 메시지 송출 """
+        default_settings = {'position': 'bottom-left', 'duration': 2000, 'closeIcon': ':/images/x.svg'}
+        modal_collection = {'info':QCustomModals.InformationModal, 
+                            'success':QCustomModals.SuccessModal, 
+                            'error':QCustomModals.ErrorModal, 
+                            'warning':QCustomModals.WarningModal, 
+                            'custom':QCustomModals.CustomModal}
+        if modal_type not in ['info', 'success', 'error', 'warning', 'custom']: modal_type = 'custom'
+
+        for key, value in kargs.items():
+            default_settings[key] = value    
+
+        modal = modal_collection[modal_type](**default_settings)
+        modal.show()
+
+
+    def open_pdf(self):
+        # 열고자 하는 PDF 파일의 경로
+        pdf_path = QFile(":/help/SHP_Coordinate_Converter_helper.pdf")
+
+        if not pdf_path.open(QFile.ReadOnly):
+            print("Failed to open resource file")
+            return
+
+        # 임시 파일에 PDF 저장
+        temp_dir = tempfile.gettempdir()
+        temp_pdf_path = os.path.join(temp_dir, "SHP_Coordinate_Converter_helper.pdf")
+        
+        with open(temp_pdf_path, 'wb') as temp_file:
+            temp_file.write(pdf_path.readAll())
+        
+        # PDF 파일을 기본 뷰어로 열기
+        if sys.platform == "win32":  # Windows
+            os.startfile(temp_pdf_path)
+        elif sys.platform == "darwin":  # macOS
+            os.system(f"open {temp_pdf_path}")
+        else:  # Linux and others
+            os.system(f"xdg-open {temp_pdf_path}")
+
     def run(self):
-        l1_s = (float(self.py1.text()), float(self.px1.text()))
-        l1_e = (float(self.py2.text()), float(self.px2.text()))
-        l2_s = (float(self.qy1.text()), float(self.qx1.text()))
-        l2_e = (float(self.qy2.text()), float(self.qx2.text()))
+        # shp파일 입력했는지 확인
+        if self.shp is None:
+            self.show_modal("error", parent=self.main_frame, title="Input SHP File Required", description="Please enter a valid shp filename to proceed.")
+            return            
 
-        dxdy = calculate_dxdy(l1_s, l2_s)
-        r1, v1 = calculate_length_and_bearing(l1_s, l1_e)
-        r2, v2 = calculate_length_and_bearing(l2_s, l2_e)
+        # 모든 좌표를 기입했는지 확인 및 변환
+        try:
+            l1_s = (float(self.py1.text()), float(self.px1.text()))
+            l1_e = (float(self.py2.text()), float(self.px2.text()))
+            l2_s = (float(self.qy1.text()), float(self.qx1.text()))
+            l2_e = (float(self.qy2.text()), float(self.qx2.text()))
+        except Exception as e:
+            self.show_modal("error", parent=self.main_frame, title="Missing Input Fields", description="Please ensure that all fields are filled out before proceeding.")
+            return
 
-        s = r2 / r1
-        dv = v2 - v1
+        # 변환량 계산
+        r1, v1 = calculate_length_and_bearing(l1_s, l1_e) # 기준선의 거리 방위각
+        r2, v2 = calculate_length_and_bearing(l2_s, l2_e) # 이동선의 거리 방위각
 
-        translation = dxdy  # x 방향으로 10만큼, y 방향으로 20만큼 이동
-        rotation_angle = dv # 45도 회전
-        scaling_factor = s # 1.5배 축척
-        rotation_origin = (l2_s[0], l2_s[1])
+        translation = calculate_dxdy(l1_s, l2_s)  # x, y 이동량
+        rotation_angle = v2 - v1 # 회전량
+        scaling_factor = r2 / r1 # 축척계수
+        rotation_origin = (l2_s[0], l2_s[1])  # 회전 기준점
 
-        adjust_shapefile_features(self.shp, self.saveas, translation, rotation_angle, scaling_factor, rotation_origin=rotation_origin)
+        # 변환 실행
+        try:
+            adjust_shapefile_features(self.shp, self.saveas, translation, rotation_angle, scaling_factor, rotation_origin=rotation_origin)
+            self.show_modal('success', parent=self.main_frame, title="Transform Success", description=f"Export: {self.saveas}")
+        except Exception as e:
+            self.show_modal("error", parent=self.main_frame, title="Transform Failed", description=f"{e}")
+
+    # ==== 위젯 이동 관련 ===============
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.m_flag = True
+            self.m_Position = event.globalPos() - self.pos()  # Get the position of the mouse relative to the window
+            event.accept()
+            self.setCursor(QCursor(Qt.OpenHandCursor))  # Change mouse icon
+
+    def mouseMoveEvent(self, event):
+        if Qt.LeftButton and self.m_flag:
+            self.move(event.globalPos() - self.m_Position)  # Change window position
+            event.accept()
+
+    def mouseReleaseEvent(self, event):
+        self.m_flag = False
+        self.setCursor(QCursor(Qt.ArrowCursor))
+    # ==================================
 
     
-"""
-    l1_s = Point(217351.37, 505549.82)
-    l1_e = Point(216828.55, 507359.74)
-    l2_s = Point(219991.27, 505193.11)
-    l2_e = Point(219657.02, 507186.06)
-"""
-
-
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    myWindow = ShpTransformer()
+    myWindow = ShpConverter()
     myWindow.show()
 
     sys.exit(app.exec_())
---
+++ shp_convert.py
@@ -0,0 +1,127 @@
+import geopandas as gpd
+from shapely.geometry import Point, base, mapping, shape
+from shapely.affinity import translate, rotate, scale
+import math
+
+def calculate_length_and_bearing(start_point: Point, end_point: Point) -> tuple: 
+    """
+    주어진 시작점과 끝점으로 선분의 길이와 방위각을 계산합니다.
+    
+    :param start_point: (shapely.geometry.Point), 객체로 시작점 좌표
+    :param end_point: (shapely.geometry.Point), 객체로 끝점 좌표
+    :return: (tuple), length, bearing 길이와 방위각(도 단위)    
+    """
+    #  tuple이면 Point객체로 변경
+    if isinstance(start_point, tuple):
+        start_point = Point(*start_point)
+    if isinstance(end_point, tuple):
+        end_point = Point(*end_point)
+    
+    # 좌표 추출
+    x1, y1 = start_point.x, start_point.y
+    x2, y2 = end_point.x, end_point.y
+    
+    # 선분의 길이 계산 (피타고라스 정리)
+    length =math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    
+    # 방위각 계산 (atan2 사용)
+    bearing = math.degrees(math.atan2(y2 - y1, x2 - x1))
+    
+    # 방위각을 0 ~ 360 범위로 조정
+    if bearing < 0:
+        bearing += 360
+    
+    return length, bearing
+
+def calculate_dxdy(start_point: Point, end_point: Point) -> tuple:
+    """
+    주어진 시작점과 끝점으로 선분의 길이와 방위각을 계산합니다.
+    
+    :param start_point: (shapely.geometry.Point), 객체로 시작점 좌표
+    :param end_point: (shapely.geometry.Point), 객체로 끝점 좌표
+    :return: (length, bearing) 길이와 방위각 (도 단위)
+    """
+    if isinstance(start_point, tuple):
+        start_point = Point(*start_point)
+    if isinstance(end_point, tuple):
+        end_point = Point(*end_point)
+
+    dx = end_point.x - start_point.x
+    dy = end_point.y - start_point.y    
+    return dx, dy
+
+def transform_geometry(geometry: base.BaseGeometry, translation: tuple=(0, 0), rotation_angle: float=0, scaling_factor: float=1.0, rotation_origin: tuple=(0, 0)) -> base.BaseGeometry:
+    """
+    주어진 기하학적 객체에 대해 이동, 회전, 축척 변환을 적용합니다.
+    
+    :param geometry: (BaseGeometry), 변환할 shapely 기하학적 객체
+    :param translation: (tuple) x, y 이동 거리 (기본값: (0, 0))
+    :param rotation_angle: (float), 회전 각도(단위: degree) (기본값: 0도)
+    :param scaling_factor: (float), 축척 배율 (기본값: 1)
+    :param rotation_origin: (tuple), 회전 기준점(기본값: (0, 0))
+    :return: (BaseGeometry), 변환된 shapely 기하학적 객체
+    """
+    # 이동 변환
+    transformed_geometry = translate(geometry, xoff=translation[0], yoff=translation[1])
+    
+    # 회전 변환 (기본값: 원점 기준 회전)
+    transformed_geometry = rotate(transformed_geometry, rotation_angle, origin=rotation_origin)
+    
+    # 축척 변환
+    transformed_geometry = scale(transformed_geometry, xfact=scaling_factor, yfact=scaling_factor, origin=rotation_origin)
+
+    # 소수점 3자리로 변환
+    transformed_geometry = round_coordinates(transformed_geometry, precision=3)
+    
+    return transformed_geometry
+
+def round_coordinates(geom: base.BaseGeometry, precision: int = 3) -> base.BaseGeometry:
+    """
+    주어진 geometry 객체의 모든 좌표를 소수점 precision 자리로 반올림합니다.
+
+    :param geom: (BaseGeometry), 변환할 shapely geometry 객체
+    :param precision: (int), 반올림할 소수점 자리 수 (기본값은 3)
+    
+    Return: BaseGeometry, 좌표가 반올림된 새로운 geometry 객체
+    """
+    if geom.is_empty:
+        return geom
+    
+    # geometry를 GeoJSON 포맷으로 변환하여 좌표 접근
+    geom_mapping = mapping(geom)
+    
+    # 좌표 반올림 처리
+    def round_elements(nested):
+        rounded = []
+        for item in nested:
+            if isinstance(item, (list, tuple)):
+                # 리스트나 튜플인 경우 재귀적으로 호출
+                rounded.append(round_elements(item))
+            else:
+                # 소수점 3째 자리에서 반올림
+                rounded.append(round(item, 3))
+        return rounded
+
+    geom_mapping['coordinates'] = round_elements(geom_mapping['coordinates'])
+    return shape(geom_mapping)
+
+def adjust_shapefile_features(input_shapefile: str, output_shapefile: str, translation: tuple=(0, 0), rotation_angle: float=0, scaling_factor: float=1.0, rotation_origin: tuple=(0, 0)):
+    """
+    Shapefile의 피처 좌표에 대해 이동, 회전, 축척 변환을 적용하여 새로운 Shapefile에 저장합니다.
+    
+    :param input_shapefile: (str), 입력 Shapefile 경로
+    :param output_shapefile: (str), 출력 Shapefile 경로
+    :param translation: (tuple), x, y 이동 거리 (기본값: (0, 0))
+    :param rotation_angle: (float), 회전 각도 (기본값: 0도)
+    :param scaling_factor: (float), 축척 배율 (기본값: 1)
+    :param rotation_origin: (tuple), 회전 기준점 (기본값: (0, 0))
+    :return: none, shp 파일 저장
+    """
+    # Shapefile 읽기
+    gdf = gpd.read_file(input_shapefile)
+    
+    # 각 피처의 기하학적 객체에 대해 변환 적용
+    gdf['geometry'] = gdf['geometry'].apply(lambda geom: transform_geometry(geom, translation, rotation_angle, scaling_factor, rotation_origin))
+
+       # 변환된 Shapefile 쓰기
+    gdf.to_file(output_shapefile, encoding='euc-kr')
